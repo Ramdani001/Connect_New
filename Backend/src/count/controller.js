9aee7b456d2ab5c1320d5636bc41fa722191c6b2
Rizkan00 <rizkan0990@gmail.com>
2024-08-31 12:05:50 +0700

    Add backend count

--- Backend/src/count/controller.js
+++ Backend/src/count/controller.js
@@ -2,7 +2,7 @@ const pool = require('../../db');
 const queries = require("./query");
 
 const getCount = (req, res) => {
-    pool.query(queries.getCart, (error, results) => {
+    pool.query(queries.getCount, (error, results) => {
         if(error) throw error.message;
         res.status(200).json(results);
     });
