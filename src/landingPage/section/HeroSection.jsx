c0939b7da7ca6a353231e035c36798efe2d1c489
Rizkan00 <rizkan0990@gmail.com>
2024-09-04 23:04:39 +0700

    D

--- src/landingPage/section/HeroSection.jsx
+++ src/landingPage/section/HeroSection.jsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
         <div id="heroSection" className="h-screen w-full bg-[url('images/2.jpg')] bg-no-repeat bg-cover grid grid-cols-1 lg:grid-cols-2 items-center">
             <div className="mt-20 md:p-14 text-center">
                 <div className="p-2 rounded-md ">
-                    <h1 className="text-2xl md:text-5xl mb-2">EDITING VIDEO</h1>
+                    <h1 className="text-2xl md:text-5xl mb-2 font-olive">EDITING VIDEO</h1>
 
                     <h3 className="text-xl font-semibold">Ready for make your video can viral</h3>
                     <h4 className="text-1xl font-bold">
