e71055787994eed64115908a4e63d0b5c0fe49f9
Rizkan00 <rizkan0990@gmail.com>
2024-09-04 23:02:40 +0700

    D

--- src/landingPage/component/Client.jsx
+++ src/landingPage/component/Client.jsx
@@ -6,28 +6,28 @@ function Client() {
             <h1 className="text-3xl font-semibold text-white mb-3">Our Client</h1>
             <div className="text-center border bg-white w-[90%] h-full p-5 rounded-tl-[3rem] rounded-br-[3rem] rounded-md ">
                 <div id="contClient" className="grid grid-cols-2 xs:grid-cols-3 md:grid-cols-4 place-items-center gap-10 lg:flex lg:justify-center lg:justify-items-center">
-                    <div className="w-24 h-24 md:w-32 md:h-32 bg-red-400 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/mey.jpg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 bg-red-400 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/mey.jpg')] bg-cover">
 
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/bumn.jpg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/bumn.jpg')] bg-cover">
                         
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md  relative shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/jerome.jpg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md  relative shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/jerome.jpg')] bg-cover">
                     
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md relative shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/jesica.jpg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md relative shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/jesica.jpg')] bg-cover">
                         
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/lucinta.jpeg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/lucinta.jpeg')] bg-cover">
                     
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/miaw.jpg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/miaw.jpg')] bg-cover">
                         
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/rans.jpeg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/rans.jpeg')] bg-cover">
                     
                     </div>
-                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud/media/connect/images/products/pertamina.jpg')] bg-cover">
+                    <div className="w-24 h-24 md:w-32 md:h-32 rounded-md shadow-md grid place-content-center bg-[url('http://www.tempat-transit.cloud:81/media/connect/images/products/pertamina.jpg')] bg-cover">
                     
                     </div>
                 </div>
