e912f800ad83fb64f2a7284b9dc92d6fd5edc27a
Rizkan00 <rizkan0990@gmail.com>
2024-09-22 00:38:42 +0700

    'Solved'

--- Backend/src/count/routes.js
+++ Backend/src/count/routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", controller.getCount);
 router.get("/years", controller.getYear);
  
-router.get("/getFilterMonth/:date", controller.getFilteMonth);
+router.get("/getFilterMonth/:date", controller.getFilterMonth);
 
 router.get("/getFilterDays/:days", controller.getFilterDays);
 
